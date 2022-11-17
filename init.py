4b5590a4ee97da4c25d87862215e489cf2b28182
antohuynh <anthony.huynh97@hotmail.com>
2022-11-17 21:20:06 +1100

    Finished init.py

---
+++ init.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+#Create instances for importing into other modules
+db = SQLAlchemy()
+
+ma = Marshmallow()
+
+bcrypt = Bcrypt()
+
+jwt = JWTManager()
